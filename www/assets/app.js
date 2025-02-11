b21d716a781af5b2b49cbd26eb5997d2f69c62b5
Dessapt-Corentin <dessaptcorentin66@gmail.com>
2025-02-11 16:53:18 +0100

    Ajout de login et logout avec style

--- www/assets/app.js
+++ www/assets/app.js
@@ -1,5 +1,7 @@
 import './styles/app.scss';
 import './styles/navbar.scss';
 import './styles/detail.scss';
+import './styles/login.scss';
+import './styles/admin.scss';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
