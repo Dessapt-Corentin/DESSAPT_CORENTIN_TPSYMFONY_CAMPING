9253dcfe217d542baead3eb3dbcf45589998cc51
GitHard <dessaptcorentin66@gmail.com>
2025-02-14 14:45:14 +0100

    Création CRUD User, affichage et style  pour toutes les vues

---
+++ www/src/Form/UserType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom :',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom de famille :',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('phone_number', TextType::class, [
+                'label' => 'Numéro de téléphone :',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse :',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email :',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe :',
+                'mapped' => false,
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 4,
+                        'minMessage' => 'le mot de passe doit contenir au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
