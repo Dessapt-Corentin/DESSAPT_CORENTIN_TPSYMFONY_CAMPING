4ae2382a8d10290472db88a9f2b16f02774ca505
Dessapt-Corentin <dessaptcorentin66@gmail.com>
2025-02-17 10:42:53 +0100

    Crud season fonctionnel avec son style et blocage des routes only ADMIN

---
+++ www/src/Form/SeasonType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Season;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('date_start', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('date_end', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
